a96ad40d1e281d3d3ab1588e752b7f24c2fb1e8c
John Vogel <john.eric.vogel@gmail.com>
2017-04-25 10:54:51 -0600

    Final Commit

--- Final Project/Successive Loop Closure- Moving Goal/param_chap6.m
+++ Final Project/Successive Loop Closure- Moving Goal/param_chap6.m
@@ -107,7 +107,7 @@ P.tau = 0.5;
 % initial conditions
 P.pn0    = 0;   	% initial North position
 P.pe0    = 0;   	% initial East position
-P.pd0    = 0;   	% initial Down position (negative altitude)
+P.pd0    = -100;   	% initial Down position (negative altitude)
 P.u0     = P.Va0;  	% initial velocity along body x-axis
 P.v0     = 0;   	% initial velocity along body y-axis
 P.w0     = 0;   	% initial velocity along body z-axis
@@ -128,7 +128,7 @@ P.x_trim = x_trim;
 % initial conditions
 P.pn0    = 0;  % initial North position
 P.pe0    = 0;  % initial East position
-P.pd0    = 0;  % initial Down position (negative altitude)
+P.pd0    = -100;  % initial Down position (negative altitude)
 P.u0     = x_trim(4);  % initial velocity along body x-axis
 P.v0     = x_trim(5);  % initial velocity along body y-axis
 P.w0     = x_trim(6);  % initial velocity along body z-axis
